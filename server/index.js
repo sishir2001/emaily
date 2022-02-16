6982fe9c12e2699a3ddccc87eb3d0b5a58b3fbf3
Jaladi Saai Sishir <54372985+sishir2001@users.noreply.github.com>
2022-02-16 11:04:55 +0530

    add : passport-google-oauth20

--- server/index.js
+++ server/index.js
@@ -3,22 +3,39 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStatergy = require("passport-google-oauth20").Strategy; // need a seperate statergy for every new organisation
+const keys = require("./config/keys");
 
 const app = express();
 
 // telling the general passport library to use specific stratergy
-// passport.use(new GoogleStatergy());
+passport.use(
+    new GoogleStatergy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: "/auth/google/callback",
+        },
+        (accessToken,refreshToken,profile,done) => {
 
-app.get("/", (req, res) => {
-    // @param req: request object to the "/" route
-    // @param res: response object that needs to be sent
-    res.send({
-        developer: "Jaladi Saai Sishir",
-        release: "1.0",
-        header: "testing",
-    });
-});
+            // ? this is the function that passports calls after requesting google for info
+            console.log(`accessToken : ${accessToken}`);
+            console.log(`refreshToken : ${refreshToken}`);
+            console.log(`profile :`);
+            console.log(profile);
+            console.log(`done : `);
+            console.log(done);
+        }
+    )
+);
 
+app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"],
+    })
+);
+
+app.get("/auth/google/callback",passport.authenticate("google"));
 
 const PORT = process.env.PORT || 3030;
 // listening on 5000 port number
